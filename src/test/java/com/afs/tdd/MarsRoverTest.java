0b5b775269d7c11a9349ca7eda4353ccb8cd3118
Jens Jovellano <jensjovellano@gmail.com>
2023-08-17 18:37:04 +0800

    test: added test for turn left when facing south

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -91,4 +91,18 @@ class MarsRoverTest {
         assertEquals(0, currentLocation.getY());
         assertEquals(Direction.EAST, currentLocation.getDirection());
     }
+    @Test
+    void should_change_to_location_0_0_E_when_executeCommand_given_0_0_South_and_command_turn_right() {
+        //Given
+        Location initialLocation = new Location(0, 0, Direction.SOUTH);
+        Command givenCommand = Command.TURN_LEFT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        //When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        //Then
+        assertEquals(0, currentLocation.getX());
+        assertEquals(0, currentLocation.getY());
+        assertEquals(Direction.EAST, currentLocation.getDirection());
+    }
 }
