52d0fc2d52d0d0ecf893ec6e076ae170356b183f
Jens Jovellano <jensjovellano@gmail.com>
2023-08-17 18:14:48 +0800

    feat: added move when direction is south

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -9,9 +9,11 @@ public class MarsRover {
     }
 
     public void executeCommand(Command givenCommand) {
-        if(givenCommand == Command.MOVE) {
-            if(location.getDirection() == Direction.NORTH){
+        if (givenCommand == Command.MOVE) {
+            if (location.getDirection() == Direction.NORTH) {
                 location.setY(location.getY() + 1);
+            } else if (location.getDirection() == Direction.SOUTH) {
+                location.setY(location.getY() - 1);
             }
         }
     }
